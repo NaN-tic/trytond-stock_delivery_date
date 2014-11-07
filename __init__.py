6cafe0f7377f3abd3f25f05c0cd2da10083a4352
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:43:57 +0100

    Cleanup tests

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-#This file is part stock_delivery_date module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part stock_delivery_date module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
 from .shipment import *
 from .sale import *
